f5e8615b6b08e1d6b323ee80dcf911074e76f5f6
CMcCown <connermccown94@gmail.com>
2022-06-10 11:12:12 -0600

    more updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
-import Auth from '../utils/auth';
-
-import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -18,22 +18,30 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-      <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-  {loading ? (
-    <div>Loading...</div>
-  ) : (
-    <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-  )}
-</div>
-{loggedIn && userData ? (
-  <div className="col-12 col-lg-3 mb-3">
-    <FriendList
-      username={userData.me.username}
-      friendCount={userData.me.friendCount}
-      friends={userData.me.friends}
-    />
-  </div>
-) : null}
+      {loggedIn && (
+      <div className="col-12 mb-3">
+        <ThoughtForm />
+      </div>
+    )}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
